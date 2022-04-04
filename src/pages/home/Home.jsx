e2c5402b16fbe328f5697a05481866ada4d7b267
shinhonggyu <zowoz8819@gmail.com>
2022-04-05 00:31:27 +0900

    sidebar, navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,17 @@
 import "./home.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="Home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        home container
+      </div>
+    </div>
+  );
 };
 
 export default Home;
